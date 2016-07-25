d16e403225aa5960fc8b83d0e99f11d5976472d4
stanley <stanleyye1996@gmail.com>
2016-07-25 11:07:09 -0700

    some refactoring

--- web-crawler.py
+++ web-crawler.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 linksVisited = {}
-linkSrcVisited = {}
 
 
 def crawl(aLink, originalBaseUrl):
@@ -22,86 +21,53 @@ def crawl(aLink, originalBaseUrl):
             exit()
         else:
             linksVisited[aLink] = 1
+            print(aLink)
+            print("Status:", req.status_code)
 
         soup = BeautifulSoup(req.text, "html.parser")
 
         for link in soup.find_all("a", href=True):
             if (link.get("href").startswith("http")):
 
-                try:
-                    linkReq = requests.get(link.get('href'))
-                except requests.exceptions.RequestException as e:
-                    print("The server couldn't fulfill the request for link:",
-                          link.get('href'), "Status:", e)
+                # if case is necessary to prevent printing
+                # same link multiple times
+                if ((link.get('href') in linksVisited) or
+                   (not(aLink.startswith(originalBaseUrl)))):
+                    pass
                 else:
-                    # if case is necessary to prevent printing
-                    # same link multiple times
-                    if ((link.get('href') in linksVisited) or
-                       (not(aLink.startswith(originalBaseUrl)))):
-                        pass
-                    else:
-                        linksVisited[link.get('href')] = 1
-                        print(link.get('href'))
-                        print("Status:", linkReq.status_code)
-                        crawl(link.get('href'), originalBaseUrl)
+                    crawl(link.get('href'), originalBaseUrl)
 
             elif (link.get('href').startswith("javascript:")):
                 pass
             else:
                 linkFullUrl = urljoin(originalBaseUrl, link.get('href'))
 
-                try:
-                    linkReq = requests.get(linkFullUrl)
-                except requests.exceptions.RequestException as e:
-                    print("The server couldn't fulfill the request for link:",
-                          linkFullUrl, "Status:", e)
+                # if case is necessary to prevent
+                # repeated printing of same link
+                if ((linkFullUrl in linksVisited) or
+                   (not(linkFullUrl.startswith(originalBaseUrl)))):
+                    pass
                 else:
-                    # if case is necessary to prevent
-                    # repeated printing of same link
-                    if ((linkFullUrl in linksVisited) or
-                       (not(linkFullUrl.startswith(originalBaseUrl)))):
-                        pass
-                    else:
-                        linksVisited[linkFullUrl] = 1
-                        print(linkFullUrl)
-                        print("Status:", linkReq.status_code)
-                        crawl(linkFullUrl, originalBaseUrl)
+                    crawl(linkFullUrl, originalBaseUrl)
 
         for sourceLink in soup.find_all(["img", "script"], src=True):
             if (sourceLink.get("src").startswith("http")):
-                try:
-                    sourceLinkReq = requests.get(sourceLink.get("src"))
-                except requests.exceptions.RequestException as e:
-                    print("The server couldn't fulfill the request for link:",
-                          sourceLink.get("src"), "Status:", e)
+
+                if ((sourceLink.get("src") in linksVisited) or
+                   (not(aLink.startswith(originalBaseUrl)))):
+                    pass
                 else:
-                    if ((sourceLink.get("src") in linkSrcVisited) or
-                       (not(aLink.startswith(originalBaseUrl)))):
-                        pass
-                    else:
-                        linkSrcVisited[sourceLink.get("src")] = 1
-                        print(sourceLink.get("src"))
-                        print("Status:", sourceLinkReq.status_code)
-                        crawl(sourceLink.get("src"), originalBaseUrl)
+                    crawl(sourceLink.get("src"), originalBaseUrl)
             else:
 
                 srcLinkFullUrl = urljoin(originalBaseUrl,
                                          sourceLink.get("src"))
 
-                try:
-                    sourceLinkReq = requests.get(srcLinkFullUrl)
-                except requests.exceptions.RequestException as e:
-                    print("The server couldn't fulfill the request for link:",
-                          srcLinkFullUrl, "Status:", e)
+                if ((srcLinkFullUrl in linksVisited) or
+                   (not(aLink.startswith(originalBaseUrl)))):
+                    pass
                 else:
-                    if ((srcLinkFullUrl in linkSrcVisited) or
-                       (not(aLink.startswith(originalBaseUrl)))):
-                        pass
-                    else:
-                        linkSrcVisited[srcLinkFullUrl] = 1
-                        print(srcLinkFullUrl)
-                        print("Status:", sourceLinkReq.status_code)
-                        crawl(srcLinkFullUrl, originalBaseUrl)
+                    crawl(srcLinkFullUrl, originalBaseUrl)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Process a domain.")
@@ -111,5 +77,4 @@ if __name__ == '__main__':
         crawl(args.link, args.link)
     else:
         crawl("http://" + args.link, "http://" + args.link)
-    print("There is a total of", len(linksVisited), "links on this domain")
-    print("There is a total of", len(linkSrcVisited), "sources on this domain")
+    print("There is a total of", len(linksVisited), "links and sources on this domain")
